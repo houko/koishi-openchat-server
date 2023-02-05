5267ea7dc8e21d6581f0bb4832b1f1888f2ef97b
Evan <suzukaze.hazuki2020@gmail.com>
2023-02-05 14:50:53 +0900

    first commit

---
+++ chat.py
@@ -0,0 +1,37 @@
+from revChatGPT.Official import Chatbot
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+
+port = 8006
+app = FastAPI()
+chatbot = Chatbot(api_key="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")
+
+
+# 只需填入你的 openai api_key 即可。一键直达 https://platform.openai.com/account/api-keys
+# 不再需要 2captcha 密钥
+# 按照 https://github.com/acheong08/ChatGPT 的说法是完全免费的，但未经证实
+
+class ChatRequest(BaseModel):
+  prompt: str
+
+
+@app.get("/ping")
+def ping():
+  return {"message": "pong"}
+
+
+@app.post("/chat")
+def chatGPT(request: ChatRequest):
+  prompt = request.prompt
+  print(prompt)
+  if prompt == "__clear__":
+    chatbot.reset()
+    return {"message": "OK"}
+  answer = chatbot.ask(prompt)["choices"][0]["text"]
+  print(answer)
+  return {"message": answer}
+
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="0.0.0.0", port=port)
